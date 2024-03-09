7255b6756fcfb49866b2fc8f87926c22498a49cd
Alex Brown <mail@alexbrown.info>
2024-03-09 07:26:21 +0000

    Show info if nothing to run; Pre-set length of params

--- timer.pp
+++ timer.pp
@@ -7,6 +7,13 @@ var
     params : array of string;
     i : integer;
 begin
+    if ParamCount = 0 then begin
+        WriteLn('timer: A shoddy knock-off of the Unix time command');
+        halt;
+    end;
+
+    SetLength(params, ParamCount - 1);
+
     for i := 2 to ParamCount do begin
         params := concat(params, [ParamStr(i)]);
     end;
