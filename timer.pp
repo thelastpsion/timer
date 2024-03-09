94255f7c490ab727211cddd1c37df3b5f047a516
Alex Brown <mail@alexbrown.info>
2024-03-09 21:12:40 +0000

    feat: Check if file exists; dos/win check .COM, .EXE and .BAT extensions (in that order)

--- timer.pp
+++ timer.pp
@@ -4,8 +4,15 @@ program timer;
 uses sysutils;
 var
     Start, Stop : TDateTime;
-    params : array of string;
+    params : Array of String;
     i : integer;
+    filename : String;
+    {$if defined(MSDOS) or defined(GO32V2)}
+    extensions : Array of String;
+    ext : String;
+    found : Boolean;
+    {$ifend}
+
 begin
     if ParamCount = 0 then begin
         WriteLn('timer: A shoddy knock-off of the Unix time command');
@@ -18,8 +25,31 @@ begin
         params := concat(params, [ParamStr(i)]);
     end;
 
+    filename := ParamStr(1);
+
+    {$if defined(MSDOS) or defined(GO32V2) or defined(WINDOWS)}
+    found := false;
+    extensions := ['', '.COM', '.EXE', '.BAT'];
+
+    for ext in extensions do
+    begin
+        if FileExists(filename + ext) then begin
+            filename += ext;
+            found := true;
+            break;
+        end;
+    end;
+
+    if not found then begin
+    {$else}
+    if not FileExists(filename) then begin;
+    {$ifend}
+        WriteLn('timer: ', filename, ' Not found.');
+        halt;
+    end;
+
     Start := Now;
-    ExecuteProcess(ParamStr(1), params);
+    ExecuteProcess(filename, params);
     Stop := Now;
     WriteLn;
     WriteLn('TIME: ', FormatDateTime('hh.nn.ss.zzz', Stop - Start));
